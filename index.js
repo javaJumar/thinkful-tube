567682612f859a8446a7381c055a5a9b75f99293
Chong Patterson <chong@thinkful.com>
2018-04-28 12:33:00 -0700

    thinkful tube appears finished

--- index.js
+++ index.js
@@ -5,19 +5,27 @@ function getDataFromApi(searchTerm, callback) {
 	const queryObj = {
 		part: 'snippet',
 		key: 'AIzaSyCgj-OBFUnpjpkPLuEHR5Je6CdXqNY2Hck',
-		q: 'searchTerm'
+		q: searchTerm
 	}
 	$.getJSON(YOUTUBE_SEARCH_URL, queryObj, callback);
 }
-; 
+
+let RESULT_HTML_TEMPLATE = (
+  `<div class='video-list'>
+     <p class='video-title'></p><a class="js-image-link" href=""><img class="js-image" src=""></a>
+  </div>
+`
+);
 
 //how to render the API results to the browser 
 function renderResult(result){
-	let template = `<div><a class="js-image-link" href=""><img class="js-image" src=""></a></div>`
-	template.find(".js-image").attr("src", result.snippet.thumbnails.medium.url);
-	template.find(".js-image-link").attr("href", 
-  	'https://www.youtube.com/watch?v=' + result.id.videoId)
-  	return template;
+  let template = $(RESULT_HTML_TEMPLATE);
+  template.find(".js-image").attr("src", result.snippet.thumbnails.medium.url);
+  template.find(".js-image-link").attr("href", 
+  'https://www.youtube.com/watch?v=' + result.id.videoId);
+  template.find('p').html(result.snippet.title);
+  console.log(result);
+  return template;
 }
 
 // show Youtube search results 
@@ -25,7 +33,6 @@ function youTubeSearchData(data){
 	const searchResults = data.items.map((item, index) => renderResult(item));
 	$('.search-results').html(searchResults);  
 	}
-}
 
 //what happens when you click the search button
 function goSubmit(){
